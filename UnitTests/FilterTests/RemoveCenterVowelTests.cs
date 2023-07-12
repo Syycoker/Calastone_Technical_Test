4f95430219923179ce4d812169679548d431fac0
Syycoker <cokersylas@gmail.com>
2023-07-12 18:06:03 +0100

    Introducing mocking to the solution.

--- UnitTests/FilterTests/RemoveCenterVowelTests.cs
+++ UnitTests/FilterTests/RemoveCenterVowelTests.cs
@@ -1,4 +1,6 @@
+using Calastone_Technical_Test.Helpers;
 using Calastone_Technical_Test.Repositories;
+using Moq;
 
 namespace UnitTests
 {
@@ -6,17 +8,22 @@ namespace UnitTests
   {
     private readonly string _filePath;
     private readonly FilterRepository _filterRepository;
+    private readonly Mock<IFileHelper> _fileHelper;
 
     public RemoveCenterVowelTests()
     {
       _filePath = "./Assets/file.txt";
       _filterRepository = new FilterRepository();
+      _fileHelper = new Mock<IFileHelper>();
     }
 
     [Theory]
     [InlineData("Alice", "")]
     public void Returns_a_single_word_with_no_middle_vowel(string input, string expectedOutput)
     {
+      _fileHelper.Setup(helper => helper.ReadFile(It.IsAny<string>()))
+        .Returns(input);
+
       var actualWords = _filterRepository.FilterInput(_filePath);
       Assert.True(actualWords.Count == expectedOutput.Length);
     }
