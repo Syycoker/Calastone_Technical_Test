3719743d8b950e18f06187c227cfdc536b6da599
Syycoker <cokersylas@gmail.com>
2023-07-12 19:23:40 +0100

    about to start testing all filters

--- UnitTests/FilterTests/WordsWithLetterTTests.cs
+++ UnitTests/FilterTests/WordsWithLetterTTests.cs
@@ -20,7 +20,7 @@ public class WordsWithLetterTTests
   [InlineData("no word", "no word")]
   [InlineData("no word with letter t", "no word")]
   [InlineData("so managed to put it into one of the cupboards as she fell past it.", "so managed one of cupboards as she fell")]
-  public void Returns_words_with_less_than_three_letters(string input, string expectedOutput)
+  public void Returns_words_with_letter_t(string input, string expectedOutput)
   {
     _fileHelper.Setup(helper => helper.ReadFile(It.IsAny<string>()))
       .Returns(input);
